a6370848565d2590aecdb0811bf4599c2591dc54
Pashkelon <pasha.gurevich1994@gmail.com>
2019-11-11 11:49:01 +0300

    commit

--- HomeWorkOne/app/src/main/java/com/example/homeworkone/MainActivity.java
+++ HomeWorkOne/app/src/main/java/com/example/homeworkone/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.homeworkone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
+        Intent intent = new Intent(this,WeatherActivity.class);
+        startActivity(intent);
     }
 }
---
+++ HomeWorkOne/app/src/main/java/com/example/homeworkone/WeatherActivity.java
@@ -0,0 +1,45 @@
+package com.example.homeworkone;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_activity);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+}
